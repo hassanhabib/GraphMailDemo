b380d89e877e4ea6cbfc8c54eebc12fbea080539
Hassan Habib <hassanhabib@live.com>
2020-10-20 22:31:21 -0700

    Interface signature

--- GraphMailDemo/Brokers/Mail/IMailBroker.cs
+++ GraphMailDemo/Brokers/Mail/IMailBroker.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace GraphMailDemo.Brokers.Mail
 {
     public interface IMailBroker
     {
-
+        ValueTask SendMail(
+            List<string> recipients, 
+            List<string> ccRecipients, 
+            string subject, 
+            string content);
     }
 }
